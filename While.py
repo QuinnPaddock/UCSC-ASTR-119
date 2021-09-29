31a870f56c0b1128c72b7194909caa39fc98ae84
Quinn Paddock <91283358+QuinnPaddock@users.noreply.github.com>
2021-09-29 15:24:55 -0700

    Add files via upload

---
+++ While.py
@@ -0,0 +1,11 @@
+
+def main():
+	i = 0
+	while (i<119):
+		print("%d is still not greater than 119." % i)
+		i += 10
+
+	#print("test") #this was just some troubleshooting
+
+if __name__ == "__main__":
+	main()
