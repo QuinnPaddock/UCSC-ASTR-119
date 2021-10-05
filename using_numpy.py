9017efabdc15dab1d8199c01b657621592db706a
Quinn Paddock <91283358+QuinnPaddock@users.noreply.github.com>
2021-10-05 11:27:46 -0700

    Add files via upload

---
+++ using_numpy.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+x = 1.0 #float x = 1
+y = 2.0 #float y = 2
+
+#trig stuff
+print(np.sin(x))
+print(np.cos(x))
+print(np.tan(x))
+print(np.arcsin(x))
+print(np.arccos(x))
+print(np.arctan(x))
+print(np.arctan2(x,y)) #arctan(x/y)
+
+
+#hyperbolic functions
+print(np.sinh(x)) #sinh(x)
+print(np.cosh(x)) #you get the idea
+print(np.tanh(x))
+print(np.arcsinh(x))
+print(np.arccosh(x))
+print(np.arctanh(x))
